a2fef0fb39b00edf271184465a386e6f59253667
pslcorrea <pslcorrea@gmail.com>
2023-09-28 18:17:32 -0300

    Correção de user do banco, implementações de detalhes da vaga

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, jsonify
-from database import carrega_vagas_db
+from database import carrega_vagas_db, carrega_vaga_db
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def home():
-  vagas = carrega_vagas_db()
-  return render_template('home.html', vagas=vagas)
+    vagas = carrega_vagas_db()
+    return render_template('home.html', vagas=vagas)
 
 
 @app.route('/vagas')
 def lista_vagas():
-  vagas = carrega_vagas_db()
-  return jsonify(vagas)
+    vagas = carrega_vagas_db()
+    return jsonify(vagas)
+
+
+@app.route('/vaga/<id>')
+def mostra_vaga(id):
+    vaga = carrega_vaga_db(id)
+    if not vaga:
+        return 'Not Found', 404
+    return render_template('detalhe_vaga.html', vaga=vaga)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-string_conexao = "mysql+pymysql://1j8gv3lra0qbi3r6pmlu:pscale_pw_Djub0t1eJ75fxVPhCRA6RTqGu0UcHVa1NG9dLJuFibS@aws.connect.psdb.cloud/carreira-python?charset=utf8mb4"
+string_conexao = "mysql+pymysql://laab3kezwc5s25pq2at5:pscale_pw_YMDbVWmngmKmPp8R0DtQxlG1z5cdvkLAzbbZyOblwp@aws.connect.psdb.cloud/carreira-python?charset=utf8mb4"
 
 engine = create_engine(
   string_conexao,
@@ -18,3 +18,16 @@ def carrega_vagas_db():
     for vaga in resultado.all():
       vagas.append(vaga._asdict())
     return vagas
+
+def carrega_vaga_db(id):
+  with engine.connect() as conn:
+    resultado = conn.execute(text(
+      f"select * from vagas where id = :val;"
+    ),
+      {"val": id}
+    )
+    vaga = resultado.mappings().all()
+    if len(vaga) == 0:
+      return None
+    else:
+      return dict(vaga[0])
