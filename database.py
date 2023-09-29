4779aa0612c99e905a816a82de6a8dfb56342203
pslcorrea <pslcorrea@gmail.com>
2023-09-29 19:52:37 +0000

    Correção string de conexão

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine, text
 import os
 
-string_conexao = os.environ['db_conexao_string']
+from sqlalchemy import create_engine, text
+
+string_conexao = os.environ["DB_CONEXAO_STRING"]
 engine = create_engine(string_conexao,
                        connect_args={"ssl": {
                            "ssl_ca": "/etc/ssl/cert.pem"
