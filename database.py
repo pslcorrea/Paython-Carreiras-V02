6c831758cd2cd4d66ee5a0636e43ca05bc38f28e
pslcorrea <pslcorrea@gmail.com>
2023-09-29 19:23:18 +0000

    Integração com o banco de dados e cryptografia da string de conexão
    Integração com o banco de dados e cryptografia da string de conexão

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
 from sqlalchemy import create_engine, text
+import os
 
-string_conexao = "mysql+pymysql://laab3kezwc5s25pq2at5:pscale_pw_YMDbVWmngmKmPp8R0DtQxlG1z5cdvkLAzbbZyOblwp@aws.connect.psdb.cloud/carreira-python?charset=utf8mb4"
+string_conexao = os.environ['db_conexao_string']
+engine = create_engine(string_conexao,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
-engine = create_engine(
-  string_conexao,
-  connect_args={
-    "ssl":{
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
 
 def carrega_vagas_db():
   with engine.connect() as conn:
@@ -19,13 +16,11 @@ def carrega_vagas_db():
       vagas.append(vaga._asdict())
     return vagas
 
+
 def carrega_vaga_db(id):
   with engine.connect() as conn:
-    resultado = conn.execute(text(
-      f"select * from vagas where id = :val;"
-    ),
-      {"val": id}
-    )
+    resultado = conn.execute(text(f"select * from vagas where id = :val;"),
+                             {"val": id})
     vaga = resultado.mappings().all()
     if len(vaga) == 0:
       return None
